0652734f06a8eba3e6bc0e79d4a2592bc69a771f
Crowdbotics <team@crowdbotics.com>
2022-02-09 12:17:32 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/bwg_field_service_a_33468/MainActivity.java
@@ -0,0 +1,15 @@
+package com.bwg_field_service_a_33468;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "bwg_field_service_a_33468";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "bwg_field_service_a_33468",
+  url: "https://bwg_field_service_a_33468.botics.co",
+  api: "https://bwg_field_service_a_33468.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
