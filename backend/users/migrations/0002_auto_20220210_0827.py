df28a504a8e4057184700c26874bf469edfce466
Crowdbotics <team@crowdbotics.com>
2022-02-10 08:30:05 +0000

    Update models

--- backend/users/migrations/0002_auto_20220210_0827.py
+++ backend/users/migrations/0002_auto_20220210_0827.py
@@ -6,43 +6,43 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('users', '0001_initial'),
+        ("users", "0001_initial"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='user',
-            name='area',
+            model_name="user",
+            name="area",
             field=models.TextField(blank=True, null=True),
         ),
         migrations.AddField(
-            model_name='user',
-            name='city',
+            model_name="user",
+            name="city",
             field=models.TextField(blank=True, null=True),
         ),
         migrations.AddField(
-            model_name='user',
-            name='door',
+            model_name="user",
+            name="door",
             field=models.IntegerField(blank=True, null=True),
         ),
         migrations.AddField(
-            model_name='user',
-            name='pincode',
+            model_name="user",
+            name="pincode",
             field=models.SmallIntegerField(blank=True, null=True),
         ),
         migrations.AddField(
-            model_name='user',
-            name='state',
+            model_name="user",
+            name="state",
             field=models.TextField(blank=True, null=True),
         ),
         migrations.AddField(
-            model_name='user',
-            name='street',
+            model_name="user",
+            name="street",
             field=models.TextField(blank=True, null=True),
         ),
         migrations.AlterField(
-            model_name='user',
-            name='name',
+            model_name="user",
+            name="name",
             field=models.CharField(blank=True, max_length=255, null=True),
         ),
     ]
